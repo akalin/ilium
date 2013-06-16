4ddb626ed5b2caa412f966d469808bca23405bbf
Frederick Akalin <akalin@gmail.com>
2013-06-16 02:44:48 -0700

    Flesh out Spectrum and RadianceMeter

--- radiance_meter.go
+++ radiance_meter.go
@@ -1,6 +1,12 @@
 package main
 
-type RadianceMeter struct{}
+import "fmt"
+
+type RadianceMeter struct {
+	n      int
+	_LMean Spectrum
+	_LM2   Spectrum
+}
 
 func MakeRadianceMeter() *RadianceMeter {
 	return &RadianceMeter{}
@@ -11,7 +17,30 @@ func (rm *RadianceMeter) GenerateRay(sensorSample SensorSample) Ray {
 }
 
 func (rm *RadianceMeter) RecordSample(sensorSample SensorSample, Li Spectrum) {
+	rm.n++
+	// delta = x - mean
+	var delta Spectrum
+	delta.Sub(&Li, &rm._LMean)
+	// mean = mean + delta/n
+	var deltaOverN Spectrum
+	deltaOverN.ScaleInv(&delta, float32(rm.n))
+	rm._LMean.Add(&rm._LMean, &deltaOverN)
+	// M2 = M2 + delta*(x - mean)
+	var t Spectrum
+	t.Sub(&Li, &rm._LMean)
+	t.Mul(&t, &delta)
+	rm._LM2.Add(&rm._LM2, &t)
 }
 
 func (rm *RadianceMeter) EmitSignal() {
+	// variance = M2/(n - 1)
+	var variance Spectrum
+	variance.ScaleInv(&rm._LM2, float32(rm.n-1))
+	var stdDev Spectrum
+	stdDev.Sqrt(&variance)
+	// standard error = standard deviation / sqrt(n)
+	var stdError Spectrum
+	stdError.ScaleInv(&stdDev, sqrtFloat32(float32(rm.n)))
+	fmt.Printf("mean=%v, std dev=%v, std error=%v\n",
+		rm._LMean, stdDev, stdError)
 }
