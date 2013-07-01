4bbaf76908ccd9b76c7d064533b2d36c93848321
Frederick Akalin <akalin@gmail.com>
2013-06-30 20:11:41 -0700

    Read config from file

--- ilium.go
+++ ilium.go
@@ -1,82 +1,24 @@
 package main
 
+import "encoding/json"
+import "io/ioutil"
 import "math/rand"
 import "time"
+import "os"
 
 func main() {
-	// The scene is a radiance meter inside a hollow sphere that
-	// both emits and reflects light.
-	//
-	// The radiance along any ray inside the sphere should
-	// converge to Le / (1 - rho), which in this case is (0.5,
-	// 0.5, 0.5).
-	//
-	// For an eye path with k bounces, the pixel values will be Le
-	// (1 - rho^k) / (1 - rho).  Thus, the absolute error is Le
-	// rho^k / (1 - rho), and the relative error is rho^k.  To get
-	// the absolute error less than eps, you need more than (lg
-	// eps + lg (1 - rho) - lg Le) / lg rho bounces, and to get
-	// the relative error less than eps, you need more than lg eps
-	// / lg rho bounces.
-	primitivesConfig := []interface{}{
-		map[string]interface{}{
-			"type": "GeometricPrimitive",
-			"shape": map[string]interface{}{
-				"type": "Sphere",
-				"center": []interface{}{
-					float64(0),
-					float64(-0.5),
-					float64(0),
-				},
-				"radius":     float64(10),
-				"flipNormal": true,
-			},
-			"material": map[string]interface{}{
-				"type":           "Diffuse",
-				"samplingMethod": "cosine",
-				"emission": map[string]interface{}{
-					"type": "rgb",
-					"r":    float64(0.1),
-					"g":    float64(0.2),
-					"b":    float64(0.4),
-				},
-				"color": map[string]interface{}{
-					"type": "rgb",
-					"r":    float64(0.8),
-					"g":    float64(0.6),
-					"b":    float64(0.2),
-				},
-			},
-		},
+	configBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
 	}
-	sceneConfig := map[string]interface{}{
-		"aggregate": map[string]interface{}{
-			"type":       "PrimitiveList",
-			"primitives": primitivesConfig,
-		},
+	var config map[string]interface{}
+	err = json.Unmarshal(configBytes, &config)
+	if err != nil {
+		panic(err)
 	}
+	sceneConfig := config["scene"].(map[string]interface{})
 	scene := MakeScene(sceneConfig)
-	rendererConfig := map[string]interface{}{
-		"type": "PathTracingRenderer",
-		"russianRouletteStartIndex": float64(10),
-		"maxEdgeCount":              float64(10),
-		"sampler": map[string]interface{}{
-			"type": "IndependentSampler",
-		},
-		"sensor": map[string]interface{}{
-			"type": "RadianceMeter",
-			"position": []interface{}{
-				float64(0),
-				float64(-0.5),
-				float64(0),
-			},
-			"target": []interface{}{
-				float64(0),
-				float64(1),
-				float64(0),
-			},
-		},
-	}
+	rendererConfig := config["renderer"].(map[string]interface{})
 	renderer := MakeRenderer(rendererConfig)
 	seed := time.Now().UTC().UnixNano()
 	rng := rand.New(rand.NewSource(seed))
