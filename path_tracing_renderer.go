4aaa037a9e4665ba24b3ef8adfdd80dea390f974
Frederick Akalin <akalin@gmail.com>
2013-07-07 13:34:54 -0700

    Render using multiple goroutines
    Feature-Start: multi-core

--- path_tracing_renderer.go
+++ path_tracing_renderer.go
@@ -27,39 +27,88 @@ func MakePathTracingRenderer(
 	return ptr
 }
 
-func (ptr *PathTracingRenderer) processPixel(
+type pathTracingBlock struct {
+	blockNumber int
+	x, y        int
+}
+
+type processedPathTracingBlock struct {
+	block        pathTracingBlock
+	_WeLiDivPdfs []Spectrum
+}
+
+func (ptr *PathTracingRenderer) processBlocks(
 	rng *rand.Rand, scene *Scene, sensor Sensor,
-	sensorSamples []Sample, x, y, i, numBlocks int) {
-	var WeLiDivPdf Spectrum
-	ptr.sampler.GenerateSamples(sensorSamples, rng)
-	fmt.Printf("Processing block %d/%d\n", i+1, numBlocks)
-	for i := 0; i < len(sensorSamples); i++ {
-		ptr.pathTracer.SampleSensorPath(
-			rng, scene, sensor, x, y, sensorSamples[i], &WeLiDivPdf)
-		sensor.RecordContribution(x, y, WeLiDivPdf)
+	inputCh chan pathTracingBlock,
+	outputCh chan processedPathTracingBlock) {
+	sensorSamples := make([]Sample, sensor.GetExtent().SamplesPerXY)
+	for block := range inputCh {
+		ptr.sampler.GenerateSamples(sensorSamples, rng)
+		WeLiDivPdfs := make([]Spectrum, len(sensorSamples))
+		for i := 0; i < len(sensorSamples); i++ {
+			ptr.pathTracer.SampleSensorPath(
+				rng, scene, sensor, block.x, block.y,
+				sensorSamples[i], &WeLiDivPdfs[i])
+		}
+		outputCh <- processedPathTracingBlock{block, WeLiDivPdfs}
 	}
 }
 
 func (ptr *PathTracingRenderer) processSensor(
-	rng *rand.Rand, scene *Scene, sensor Sensor) {
+	numRenderJobs int, rng *rand.Rand, scene *Scene, sensor Sensor) {
+	blockCh := make(chan pathTracingBlock, numRenderJobs)
+	defer close(blockCh)
+	processedBlockCh := make(chan processedPathTracingBlock, numRenderJobs)
+	for i := 0; i < numRenderJobs; i++ {
+		workerRng := rand.New(rand.NewSource(rng.Int63()))
+		go ptr.processBlocks(
+			workerRng, scene, sensor, blockCh, processedBlockCh)
+	}
+
 	sensorExtent := sensor.GetExtent()
 	numBlocks := sensorExtent.GetPixelCount()
-	sensorSamples := make([]Sample, sensorExtent.SamplesPerXY)
-	i := 0
-	for x := sensorExtent.XStart; x < sensorExtent.XEnd; x++ {
-		for y := sensorExtent.YStart; y < sensorExtent.YEnd; y++ {
-			ptr.processPixel(
-				rng, scene, sensor, sensorSamples,
-				x, y, i, numBlocks)
-			i++
+	recordWeLiDivPdfSamples := func(
+		processedBlock processedPathTracingBlock) {
+		block := processedBlock.block
+		fmt.Printf("Finished block %d/%d\n",
+			block.blockNumber+1, numBlocks)
+		for i := 0; i < len(processedBlock._WeLiDivPdfs); i++ {
+			sensor.RecordContribution(
+				block.x, block.y,
+				processedBlock._WeLiDivPdfs[i])
 		}
 	}
+
+	processed := 0
+	xCount := sensorExtent.GetXCount()
+	for blockNumber := 0; blockNumber < numBlocks; {
+		select {
+		case processedBlock := <-processedBlockCh:
+			recordWeLiDivPdfSamples(processedBlock)
+			processed++
+		default:
+			fmt.Printf("Queueing block %d/%d\n",
+				blockNumber+1, numBlocks)
+			x := sensorExtent.XStart + blockNumber%xCount
+			y := sensorExtent.YStart + blockNumber/xCount
+			blockCh <- pathTracingBlock{blockNumber, x, y}
+			blockNumber++
+		}
+	}
+
+	for processed < numBlocks {
+		processedBlock := <-processedBlockCh
+		recordWeLiDivPdfSamples(processedBlock)
+		processed++
+	}
+
 	sensor.EmitSignal()
 }
 
-func (ptr *PathTracingRenderer) Render(rng *rand.Rand, scene *Scene) {
+func (ptr *PathTracingRenderer) Render(
+	numRenderJobs int, rng *rand.Rand, scene *Scene) {
 	sensors := scene.Aggregate.GetSensors()
 	for _, sensor := range sensors {
-		ptr.processSensor(rng, scene, sensor)
+		ptr.processSensor(numRenderJobs, rng, scene, sensor)
 	}
 }
