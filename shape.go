b7e18b8ec0076d72026c2795703dce1ff3ee1d8f
Frederick Akalin <akalin@gmail.com>
2013-07-10 01:13:10 -0700

    Implement Rect shape
    Feature-Start: rect

--- shape.go
+++ shape.go
@@ -12,6 +12,8 @@ func MakeShape(config map[string]interface{}) Shape {
 	switch shapeType {
 	case "Sphere":
 		return MakeSphere(config)
+	case "Rect":
+		return MakeRect(config)
 	default:
 		panic("unknown shape type " + shapeType)
 	}
