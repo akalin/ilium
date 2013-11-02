672a0f1d74fd30723889f9c35705bc1fec9d2876
Frederick Akalin <akalin@gmail.com>
2013-11-01 23:35:00 -0700

    Implement reading the config for a TriangleMesh

--- ilium/point3.go
+++ ilium/point3.go
@@ -6,6 +6,17 @@ func MakePoint3FromConfig(config interface{}) Point3 {
 	return Point3(MakeR3FromConfig(config))
 }
 
+func MakePoint3sFromConfig(config interface{}) []Point3 {
+	arrayConfig := config.([]interface{})
+	points := []Point3{}
+	for i := 0; i < len(arrayConfig); i += 3 {
+		points = append(
+			points,
+			Point3(MakeR3FromConfig(arrayConfig[i:i+3])))
+	}
+	return points
+}
+
 func (out *Point3) Shift(p *Point3, offset *Vector3) {
 	((*R3)(out)).Add((*R3)(p), (*R3)(offset))
 }
--- ilium/triangle_mesh.go
+++ ilium/triangle_mesh.go
@@ -1,10 +1,41 @@
 package ilium
 
+type triangleMesh struct {
+	vertices []Point3
+	indices  [][3]int
+}
+
 type Triangle struct {
+	mesh *triangleMesh
+	i    int
+}
+
+func makeIndicesFromConfig(config interface{}) [][3]int {
+	arrayConfig := config.([]interface{})
+	indices := [][3]int{}
+	for i := 0; i < len(arrayConfig); i += 3 {
+		indices = append(
+			indices,
+			[3]int{
+				int(arrayConfig[i+0].(float64)),
+				int(arrayConfig[i+1].(float64)),
+				int(arrayConfig[i+2].(float64)),
+			})
+	}
+	return indices
 }
 
 func MakeTriangleMesh(config map[string]interface{}) []Shape {
-	return []Shape{&Triangle{}}
+	verticesConfig := config["vertices"].([]interface{})
+	vertices := MakePoint3sFromConfig(verticesConfig)
+	indicesConfig := config["indices"].([]interface{})
+	indices := makeIndicesFromConfig(indicesConfig)
+	mesh := triangleMesh{vertices, indices}
+	triangles := make([]Shape, len(indices))
+	for i := 0; i < len(triangles); i++ {
+		triangles[i] = &Triangle{&mesh, i}
+	}
+	return triangles
 }
 
 func (tr *Triangle) Intersect(ray *Ray, intersection *Intersection) bool {
