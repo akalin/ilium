65b5465d4634243c348a078ce0890865cf85bea6
Frederick Akalin <akalin@gmail.com>
2013-11-02 00:28:53 -0700

    Implement triangle intersection
    Also implement cross product for vectors and normals.

--- ilium/normal3.go
+++ ilium/normal3.go
@@ -6,6 +6,10 @@ func (out *Normal3) Flip(n *Normal3) {
 	((*R3)(out)).Invert((*R3)(n))
 }
 
+func (out *Normal3) CrossVectorNoAlias(v, w *Vector3) {
+	((*R3)(out)).CrossNoAlias((*R3)(v), (*R3)(w))
+}
+
 func (out *Normal3) Normalize(n *Normal3) {
 	((*R3)(out)).Normalize((*R3)(n))
 }
