f63afd61fe5262add18d4bc254ad513b4bf60973
Frederick Akalin <akalin@gmail.com>
2013-11-02 21:26:06 -0700

    Move sampling functions to sampling.go

--- ilium/diffuse_material.go
+++ ilium/diffuse_material.go
@@ -1,7 +1,5 @@
 package ilium
 
-import "math"
-
 type DiffuseMaterialSamplingMethod int
 
 const (
@@ -30,41 +28,12 @@ func MakeDiffuseMaterial(config map[string]interface{}) *DiffuseMaterial {
 	return &DiffuseMaterial{samplingMethod, color}
 }
 
-func uniformSampleDisk(u1, u2 float32) (x, y float32) {
-	// This has a slight bias towards the center.
-	r := sqrtFloat32(u1)
-	theta := 2 * math.Pi * u2
-	sinTheta, cosTheta := sincosFloat32(theta)
-	x = r * cosTheta
-	y = r * sinTheta
-	return
-}
-
-func uniformSampleSphere(u1, u2 float32) R3 {
-	// This has a slight bias towards the top of the sphere.
-	cosTheta := 1 - 2*u1
-	sinTheta := sqrtFloat32(maxFloat32(0, 1-cosTheta*cosTheta))
-	phi := 2 * math.Pi * u2
-	sinPhi, cosPhi := sincosFloat32(phi)
-	return R3{
-		sinTheta * cosPhi,
-		sinTheta * sinPhi,
-		cosTheta,
-	}
-}
-
-func cosineSampleHemisphere(u1, u2 float32) R3 {
-	// This has a slight bias towards the top of the hemisphere.
-	x, y := uniformSampleDisk(u1, u2)
-	z := sqrtFloat32(maxFloat32(0, 1-x*x-y*y))
-	return R3{x, y, z}
-}
-
 func (d *DiffuseMaterial) SampleWi(u1, u2 float32, wo Vector3, n Normal3) (
 	wi Vector3, fDivPdf Spectrum) {
 	switch d.samplingMethod {
 	case DIFFUSE_MATERIAL_UNIFORM_SAMPLING:
-		wi = Vector3(uniformSampleSphere(u1, u2))
+		r := uniformSampleSphere(u1, u2)
+		wi = Vector3(r)
 		absCosTh := absFloat32(wi.DotNormal(&n))
 		// f = color / pi and pdf = 1 / (2 * pi * |cos(th)|), so
 		// f / pdf = 2 * color * |cos(th)|.
