5e0e97382707e308129dcbcefef96a925f97dac5
Frederick Akalin <akalin@gmail.com>
2013-11-08 17:21:31 -0800

    Pull out shape-sampling functionality into shapeSet type

---
+++ ilium/shape_set.go
@@ -0,0 +1,31 @@
+package ilium
+
+type shapeSet struct {
+	shapes                []Shape
+	shapeAreaDistribution Distribution1D
+}
+
+func MakeShapeSet(shapes []Shape) shapeSet {
+	shapeAreas := make([]float32, len(shapes))
+	for i := 0; i < len(shapes); i++ {
+		shapeAreas[i] = shapes[i].SurfaceArea()
+	}
+	shapeAreaDistribution := MakeDistribution1D(shapeAreas)
+	return shapeSet{shapes, shapeAreaDistribution}
+}
+
+func (ss *shapeSet) SampleSurfaceFromPoint(
+	u, v1, v2 float32, p Point3, n Normal3) (
+	pSurface Point3, nSurface Normal3, pdfProjectedSolidAngle float32) {
+	i, pShape := ss.shapeAreaDistribution.SampleDiscrete(u)
+	shape := ss.shapes[i]
+	pSurface, nSurface, pdfShape :=
+		shape.SampleSurfaceFromPoint(v1, v2, p, n)
+	if pdfShape == 0 {
+		return
+	}
+	// TODO(akalin): Add an option to check for a shape with a
+	// closer intersection and use that point instead.
+	pdfProjectedSolidAngle = pShape * pdfShape
+	return
+}
