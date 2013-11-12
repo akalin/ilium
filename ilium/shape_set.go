f268bd6c8b1f9e4bbc4939f42658cf368e9c3897
Frederick Akalin <akalin@gmail.com>
2013-11-12 01:25:31 -0800

    Implement ComputePdfFromPoint() for shapeSet

--- ilium/shape_set.go
+++ ilium/shape_set.go
@@ -2,16 +2,21 @@ package ilium
 
 type shapeSet struct {
 	shapes                []Shape
+	shapeAreas            []float32
+	totalArea             float32
 	shapeAreaDistribution Distribution1D
 }
 
 func MakeShapeSet(shapes []Shape) shapeSet {
 	shapeAreas := make([]float32, len(shapes))
+	var totalArea float32
 	for i := 0; i < len(shapes); i++ {
-		shapeAreas[i] = shapes[i].SurfaceArea()
+		area := shapes[i].SurfaceArea()
+		shapeAreas[i] = area
+		totalArea += area
 	}
 	shapeAreaDistribution := MakeDistribution1D(shapeAreas)
-	return shapeSet{shapes, shapeAreaDistribution}
+	return shapeSet{shapes, shapeAreas, totalArea, shapeAreaDistribution}
 }
 
 func (ss *shapeSet) SampleSurface(u, v1, v2 float32) (
@@ -40,4 +45,19 @@ func (ss *shapeSet) SampleSurfaceFromPoint(
 	// closer intersection and use that point instead.
 	pdfProjectedSolidAngle = pShape * pdfShape
 	return
+}
+
+func (ss *shapeSet) ComputePdfFromPoint(
+	p Point3, pEpsilon float32, n Normal3, wi Vector3) float32 {
+	if ss.totalArea == 0 {
+		return 0
+	}
+	var weightedPdf float32 = 0
+	for i := 0; i < len(ss.shapes); i++ {
+		shape := ss.shapes[i]
+		area := ss.shapeAreas[i]
+		shapePdf := shape.ComputePdfFromPoint(p, pEpsilon, n, wi)
+		weightedPdf += area * shapePdf
+	}
+	return weightedPdf / ss.totalArea
 }
