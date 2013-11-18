27e63d2f8749f503ce00510b8697c65f3d02564f
Frederick Akalin <akalin@gmail.com>
2013-11-19 04:07:14 +0900

    Add DoubleCheckPrimitive aggregate type
    Also add IsCloseTo() method to Intersection.

---
+++ double_check_primitive.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type DoubleCheckPrimitive struct {
+	primary, secondary Primitive
+	epsilon            float32
+}
+
+func (dcp *DoubleCheckPrimitive) Intersect(
+	ray *Ray, intersection *Intersection) bool {
+	var primaryIntersection, secondaryIntersection Intersection
+	primaryFound := dcp.primary.Intersect(ray, &primaryIntersection)
+	secondaryFound := dcp.secondary.Intersect(ray, &secondaryIntersection)
+	if primaryFound != secondaryFound {
+		fmt.Printf(
+			"ray=%v: primary found intersection: %v, "+
+				"secondary found intersection: %v\n",
+			*ray, primaryFound, secondaryFound)
+	}
+	if !primaryIntersection.IsCloseTo(&secondaryIntersection, dcp.epsilon) {
+		fmt.Printf(
+			"ray=%v: primary intersection: %v, "+
+				"secondary intersection: %v\n",
+			*ray, primaryIntersection, secondaryIntersection)
+	}
+	*intersection = primaryIntersection
+	return primaryFound
+}
+
+func (dcp *DoubleCheckPrimitive) GetSensors() []Sensor {
+	primarySensors := dcp.primary.GetSensors()
+	secondarySensors := dcp.secondary.GetSensors()
+	if len(primarySensors) != len(secondarySensors) {
+		fmt.Printf(
+			"primary has %d sensor(s), "+
+				"secondary has %d sensor(s)\n",
+			len(primarySensors), len(secondarySensors))
+	}
+	return primarySensors
+}
+
+func MakeDoubleCheckPrimitive(
+	config map[string]interface{}, primitives []Primitive) Aggregate {
+	primaryConfig := config["primary"].(map[string]interface{})
+	secondaryConfig := config["secondary"].(map[string]interface{})
+	epsilon := float32(config["epsilon"].(float64))
+	primary := MakeAggregateWithPrimitives(primaryConfig, primitives)
+	secondary := MakeAggregateWithPrimitives(secondaryConfig, primitives)
+	return &DoubleCheckPrimitive{primary, secondary, epsilon}
+}
