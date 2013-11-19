22119470b6fd8e5f99ec0b9727ede7c45eb675ad
Frederick Akalin <akalin@gmail.com>
2013-11-20 01:38:39 +0900

    Implement CanIntersect

--- point_primitive.go
+++ point_primitive.go
@@ -9,6 +9,10 @@ func (pp *PointPrimitive) Intersect(ray *Ray, intersection *Intersection) bool {
 	return false
 }
 
+func (pp *PointPrimitive) MayIntersectBoundingBox(boundingBox BBox) bool {
+	return boundingBox.Contains(pp.location)
+}
+
 func (pp *PointPrimitive) GetBoundingBox() BBox {
 	return BBox{pp.location, pp.location}
 }
