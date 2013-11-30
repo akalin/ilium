c7a0418b21d2706cac75d8f8becd2720d6bf1475
Frederick Akalin <akalin@gmail.com>
2013-11-30 14:45:55 -0600

    Add more scaffolding to ParticleTracingRenderer and ParticleTracer
    Determine sample count from sensors, and also handle the emitInterval
    option.

--- ilium/particle_tracing_renderer.go
+++ ilium/particle_tracing_renderer.go
@@ -1,5 +1,6 @@
 package ilium
 
+import "fmt"
 import "math/rand"
 
 // A ParticleTracingRenderer uses samples from its sampler to trace
@@ -12,6 +13,22 @@ type ParticleTracingRenderer struct {
 
 func MakeParticleTracingRenderer(
 	config map[string]interface{}) *ParticleTracingRenderer {
+	var russianRouletteContribution ParticleTracerRRContribution
+	if russianRouletteContributionConfig, ok :=
+		config["russianRouletteContribution"].(string); ok {
+		switch russianRouletteContributionConfig {
+		case "alpha":
+			russianRouletteContribution = PARTICLE_TRACER_RR_ALPHA
+		case "albedo":
+			russianRouletteContribution = PARTICLE_TRACER_RR_ALBEDO
+		default:
+			panic("unknown Russian roulette contribution " +
+				russianRouletteContributionConfig)
+		}
+	} else {
+		russianRouletteContribution = PARTICLE_TRACER_RR_ALPHA
+	}
+
 	russianRouletteState := MakeRussianRouletteState(config)
 
 	maxEdgeCount := int(config["maxEdgeCount"].(float64))
@@ -29,21 +46,65 @@ func MakeParticleTracingRenderer(
 		sampler:      sampler,
 	}
 	ptr.particleTracer.InitializeParticleTracer(
-		russianRouletteState, maxEdgeCount)
+		russianRouletteContribution, russianRouletteState,
+		maxEdgeCount)
 	return ptr
 }
 
 func (ptr *ParticleTracingRenderer) processSamples(
-	numRenderJobs int, rng *rand.Rand, scene *Scene, sampleCount int) {
+	numRenderJobs int, rng *rand.Rand, scene *Scene, sensors []Sensor,
+	lightConfig SampleConfig, sampleCount int,
+	outputDir, outputExt string) {
+	// TODO(akalin): Implement blocking to avoid using too much
+	// memory when sampleCount is large.
+	particleTracerConfig := ptr.particleTracer.GetSampleConfig()
+	particleTracerSampleStorage := ptr.sampler.AllocateSampleStorage(
+		particleTracerConfig, sampleCount)
+	tracerBundles := ptr.sampler.GenerateSampleBundles(
+		particleTracerConfig,
+		particleTracerSampleStorage, sampleCount, rng)
+
+	lightSampleStorage := ptr.sampler.AllocateSampleStorage(
+		lightConfig, sampleCount)
+	lightBundles := ptr.sampler.GenerateSampleBundles(
+		lightConfig, lightSampleStorage, sampleCount, rng)
+
+	progressInterval := (sampleCount + 99) / 100
+	for i := 0; i < sampleCount; i++ {
+		ptr.particleTracer.SampleLightPath(
+			rng, scene, lightBundles[i], tracerBundles[i])
+
+		if (i+1)%progressInterval == 0 || i+1 == sampleCount {
+			fmt.Printf("Processed %d/%d sample(s)\n",
+				i+1, sampleCount)
+		}
+
+		if ((i + 1) == sampleCount) ||
+			(ptr.emitInterval > 0 &&
+				(i+1)%ptr.emitInterval == 0) {
+			for _, sensor := range sensors {
+				sensor.EmitSignal(outputDir, outputExt)
+			}
+		}
+	}
 }
 
 func (ptr *ParticleTracingRenderer) Render(
 	numRenderJobs int, rng *rand.Rand, scene *Scene,
 	outputDir, outputExt string) {
-	ptr.processSamples(numRenderJobs, rng, scene, 0)
-	// TODO(akalin): Emit signal according to emitInterval.
+	var combinedLightConfig SampleConfig
+	for _, light := range scene.Lights {
+		lightConfig := light.GetSampleConfig()
+		combinedLightConfig.CombineWith(&lightConfig)
+	}
+
+	var totalSampleCount int
 	sensors := scene.Aggregate.GetSensors()
 	for _, sensor := range sensors {
-		sensor.EmitSignal(outputDir, outputExt)
+		extent := sensor.GetExtent()
+		totalSampleCount += extent.GetSampleCount()
 	}
+
+	ptr.processSamples(numRenderJobs, rng, scene, sensors,
+		combinedLightConfig, totalSampleCount, outputDir, outputExt)
 }
