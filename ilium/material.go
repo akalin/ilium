c662c129854d14cb69b808b2b06fa2bc16f82039
Frederick Akalin <akalin@gmail.com>
2013-11-30 16:51:09 -0600

    Add parameter for transport type to Material

--- ilium/material.go
+++ ilium/material.go
@@ -1,10 +1,20 @@
 package ilium
 
+type MaterialTransportType int
+
+const (
+	MATERIAL_LIGHT_TRANSPORT      MaterialTransportType = iota
+	MATERIAL_IMPORTANCE_TRANSPORT MaterialTransportType = iota
+)
+
 type Material interface {
-	SampleWi(u1, u2 float32, wo Vector3, n Normal3) (
+	SampleWi(transportType MaterialTransportType,
+		u1, u2 float32, wo Vector3, n Normal3) (
 		wi Vector3, fDivPdf Spectrum, pdf float32)
-	ComputeF(wo, wi Vector3, n Normal3) Spectrum
-	ComputePdf(wo, wi Vector3, n Normal3) float32
+	ComputeF(transportType MaterialTransportType,
+		wo, wi Vector3, n Normal3) Spectrum
+	ComputePdf(transportType MaterialTransportType,
+		wo, wi Vector3, n Normal3) float32
 }
 
 func MakeMaterial(config map[string]interface{}) Material {
