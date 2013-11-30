26d5c323d9282950938c8d32d0ed623911c749e1
Frederick Akalin <akalin@gmail.com>
2013-11-30 14:45:55 -0600

    Add more scaffolding to ParticleTracingRenderer and ParticleTracer
    Determine sample count from sensors, and also handle the emitInterval
    option.

--- ilium/particle_tracer.go
+++ ilium/particle_tracer.go
@@ -1,12 +1,130 @@
 package ilium
 
+import "fmt"
+import "math/rand"
+
+type ParticleTracerRRContribution int
+
+const (
+	PARTICLE_TRACER_RR_ALPHA  ParticleTracerRRContribution = iota
+	PARTICLE_TRACER_RR_ALBEDO ParticleTracerRRContribution = iota
+)
+
 type ParticleTracer struct {
-	russianRouletteState *RussianRouletteState
-	maxEdgeCount         int
+	russianRouletteContribution ParticleTracerRRContribution
+	russianRouletteState        *RussianRouletteState
+	maxEdgeCount                int
 }
 
 func (pt *ParticleTracer) InitializeParticleTracer(
+	russianRouletteContribution ParticleTracerRRContribution,
 	russianRouletteState *RussianRouletteState, maxEdgeCount int) {
+	pt.russianRouletteContribution = russianRouletteContribution
 	pt.russianRouletteState = russianRouletteState
 	pt.maxEdgeCount = maxEdgeCount
+}
+
+func (pt *ParticleTracer) GetSampleConfig() SampleConfig {
+	if pt.maxEdgeCount <= 0 {
+		return SampleConfig{}
+	}
+
+	// maxVertexCount = maxEdgeCount + 1, and there are two
+	// non-interior vertices (or one in the degenerate case).
+	maxInteriorVertexCount := maxInt(0, pt.maxEdgeCount-1)
+	// Sample wi for each interior vertex to build the next edge
+	// of the path.
+	numWiSamples := minInt(3, maxInteriorVertexCount)
+	return SampleConfig{
+		Sample1DLengths: []int{
+			// One to pick the light.
+			1,
+		},
+		Sample2DLengths: []int{numWiSamples},
+	}
+}
+
+func (pt *ParticleTracer) SampleLightPath(
+	rng *rand.Rand, scene *Scene, lightBundle, tracerBundle SampleBundle) {
+	if pt.maxEdgeCount <= 0 {
+		return
+	}
+
+	if len(scene.Lights) == 0 {
+		return
+	}
+
+	u := tracerBundle.Samples1D[0][0]
+	light, pChooseLight := scene.SampleLight(u.U)
+
+	initialRay, LeDivPdf := light.SampleRay(lightBundle)
+	if LeDivPdf.IsBlack() {
+		return
+	}
+
+	LeDivPdf.ScaleInv(&LeDivPdf, pChooseLight)
+
+	wiSamples := tracerBundle.Samples2D[0]
+	ray := initialRay
+	// alpha = Le * T(path) / pdf.
+	alpha := LeDivPdf
+	albedo := LeDivPdf
+	var t *Spectrum
+	switch pt.russianRouletteContribution {
+	case PARTICLE_TRACER_RR_ALPHA:
+		t = &alpha
+	case PARTICLE_TRACER_RR_ALBEDO:
+		t = &albedo
+	}
+	var edgeCount int
+	for {
+		pContinue := pt.russianRouletteState.GetContinueProbability(
+			edgeCount, t)
+		if pContinue <= 0 {
+			break
+		}
+		if pContinue < 1 {
+			if randFloat32(rng) > pContinue {
+				break
+			}
+			alpha.ScaleInv(&alpha, pContinue)
+		}
+		var intersection Intersection
+		if !scene.Aggregate.Intersect(&ray, &intersection) {
+			break
+		}
+		// The new edge is between ray.O and intersection.P.
+		edgeCount++
+
+		var wo Vector3
+		wo.Flip(&ray.D)
+
+		// TODO(akalin): Accumulate alpha into any hit sensors.
+
+		if edgeCount >= pt.maxEdgeCount {
+			break
+		}
+
+		sampleIndex := edgeCount - 1
+		u := wiSamples.GetSample(sampleIndex, rng)
+		wi, fDivPdf, pdf := intersection.Material.SampleWi(
+			MATERIAL_IMPORTANCE_TRANSPORT,
+			u.U1, u.U2, wo, intersection.N)
+		if fDivPdf.IsBlack() || pdf == 0 {
+			break
+		}
+		if !fDivPdf.IsValid() {
+			fmt.Printf("Invalid fDivPdf %v returned for "+
+				"intersection %v and wo %v\n",
+				fDivPdf, intersection, wo)
+			break
+		}
+
+		ray = Ray{
+			intersection.P, wi,
+			intersection.PEpsilon, infFloat32(+1),
+		}
+		alpha.Mul(&alpha, &fDivPdf)
+		albedo = fDivPdf
+	}
 }
