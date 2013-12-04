b6189d931c3481f1d44d468a57dc61bbd575deaf
Frederick Akalin <akalin@gmail.com>
2013-12-04 05:10:07 -0800

    Add Light methods to sample/compute spatial and directional Le seperately
    Also implement them for DiffuseAreaLight.

--- ilium/diffuse_area_light.go
+++ ilium/diffuse_area_light.go
@@ -40,6 +40,55 @@ func (d *DiffuseAreaLight) GetSampleConfig() SampleConfig {
 	}
 }
 
+func (d *DiffuseAreaLight) SampleSurface(sampleBundle SampleBundle) (
+	pSurface Point3, pSurfaceEpsilon float32,
+	nSurface Normal3, LeSpatialDivPdf Spectrum) {
+	u := sampleBundle.Samples1D[0][0].U
+	v1 := sampleBundle.Samples2D[0][0].U1
+	v2 := sampleBundle.Samples2D[0][0].U2
+	pSurface, pSurfaceEpsilon, nSurface, pdf :=
+		d.shapeSet.SampleSurface(u, v1, v2)
+	LeSpatial := d.ComputeLeSpatial(pSurface)
+	LeSpatialDivPdf.ScaleInv(&LeSpatial, pdf)
+	return
+}
+
+func (d *DiffuseAreaLight) sampleHemisphere(nSurface Normal3, u1, u2 float32) (
+	wo Vector3, absCosTh float32) {
+	var wR3 R3
+	switch d.samplingMethod {
+	case DAL_UNIFORM_SAMPLING:
+		wR3 = uniformSampleHemisphere(u1, u2)
+	case DAL_COSINE_SAMPLING:
+		wR3 = cosineSampleHemisphere(u1, u2)
+	}
+	k := R3(nSurface)
+	var i, j R3
+	MakeCoordinateSystemNoAlias(&k, &i, &j)
+	var wR3w R3
+	wR3w.ConvertToCoordinateSystemNoAlias(&wR3, &i, &j, &k)
+	wo = Vector3(wR3w)
+	absCosTh = wR3.Z
+	return
+}
+
+func (d *DiffuseAreaLight) SampleDirection(
+	sampleBundle SampleBundle, pSurface Point3, nSurface Normal3) (
+	wo Vector3, LeDirectionalDivPdf Spectrum) {
+	w1 := sampleBundle.Samples2D[1][0].U1
+	w2 := sampleBundle.Samples2D[1][0].U2
+	wo, absCosTh := d.sampleHemisphere(nSurface, w1, w2)
+	switch d.samplingMethod {
+	case DAL_UNIFORM_SAMPLING:
+		// LeDirectional = 1 / pi and pdf = 1 / (2 * pi * |cos(th)|).
+		LeDirectionalDivPdf = MakeConstantSpectrum(2 * absCosTh)
+	case DAL_COSINE_SAMPLING:
+		// LeDirectional = 1 / pi and pdf = 1 / pi.
+		LeDirectionalDivPdf = MakeConstantSpectrum(1)
+	}
+	return
+}
+
 func (d *DiffuseAreaLight) SampleRay(sampleBundle SampleBundle) (
 	ray Ray, LeDivPdf Spectrum) {
 	u := sampleBundle.Samples1D[0][0].U
@@ -49,25 +98,17 @@ func (d *DiffuseAreaLight) SampleRay(sampleBundle SampleBundle) (
 	w2 := sampleBundle.Samples2D[1][0].U2
 	pSurface, pSurfaceEpsilon, nSurface, pdfSurfaceArea :=
 		d.shapeSet.SampleSurface(u, v1, v2)
-	var wR3 R3
+	wo, absCosTh := d.sampleHemisphere(nSurface, w1, w2)
+	ray = Ray{pSurface, wo, pSurfaceEpsilon, infFloat32(+1)}
 	switch d.samplingMethod {
 	case DAL_UNIFORM_SAMPLING:
-		wR3 = uniformSampleHemisphere(w1, w2)
-		absCosTh := wR3.Z
 		// pdf = pdfSurfaceArea / (2 * pi * |cos(th)|).
 		LeDivPdf.Scale(
 			&d.emission, 2*math.Pi*absCosTh/pdfSurfaceArea)
 	case DAL_COSINE_SAMPLING:
-		wR3 = cosineSampleHemisphere(w1, w2)
 		// pdf = pdfSurfaceArea / pi.
 		LeDivPdf.Scale(&d.emission, math.Pi/pdfSurfaceArea)
 	}
-	k := R3(nSurface)
-	var i, j R3
-	MakeCoordinateSystemNoAlias(&k, &i, &j)
-	var wR3w R3
-	wR3w.ConvertToCoordinateSystemNoAlias(&wR3, &i, &j, &k)
-	ray = Ray{pSurface, Vector3(wR3w), pSurfaceEpsilon, infFloat32(+1)}
 	return
 }
 
@@ -91,6 +132,20 @@ func (d *DiffuseAreaLight) SampleLeFromPoint(
 func (d *DiffuseAreaLight) ComputeLePdfFromPoint(
 	p Point3, pEpsilon float32, n Normal3, wi Vector3) float32 {
 	return d.shapeSet.ComputePdfFromPoint(p, pEpsilon, n, wi)
+}
+
+func (d *DiffuseAreaLight) ComputeLeSpatial(pSurface Point3) Spectrum {
+	var LeSpatial Spectrum
+	LeSpatial.Scale(&d.emission, math.Pi)
+	return LeSpatial
+}
+
+func (d *DiffuseAreaLight) ComputeLeDirectional(
+	pSurface Point3, nSurface Normal3, wo Vector3) Spectrum {
+	if wo.DotNormal(&nSurface) < 0 {
+		return Spectrum{}
+	}
+	return MakeConstantSpectrum(1 / math.Pi)
 }
 
 func (d *DiffuseAreaLight) ComputeLe(
--- ilium/light.go
+++ ilium/light.go
@@ -2,12 +2,21 @@ package ilium
 
 type Light interface {
 	GetSampleConfig() SampleConfig
+	SampleSurface(sampleBundle SampleBundle) (
+		pSurface Point3, pSurfaceEpsilon float32,
+		nSurface Normal3, LeSpatialDivPdf Spectrum)
+	SampleDirection(
+		sampleBundle SampleBundle, pSurface Point3, nSurface Normal3) (
+		wo Vector3, LeDirectionalDivPdf Spectrum)
 	SampleRay(sampleBundle SampleBundle) (ray Ray, LeDivPdf Spectrum)
 	SampleLeFromPoint(
 		u, v1, v2 float32, p Point3, pEpsilon float32, n Normal3) (
 		LeDivPdf Spectrum, pdf float32, wi Vector3, shadowRay Ray)
 	ComputeLePdfFromPoint(
 		p Point3, pEpsilon float32, n Normal3, wi Vector3) float32
+	ComputeLeSpatial(pSurface Point3) Spectrum
+	ComputeLeDirectional(
+		pSurface Point3, nSurface Normal3, wo Vector3) Spectrum
 	ComputeLe(pSurface Point3, nSurface Normal3, wo Vector3) Spectrum
 }
 
