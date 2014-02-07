cb15ac860da670af55ae4adf891c4c0b06eab749
Frederick Akalin <akalin@gmail.com>
2014-02-07 01:33:07 -0800

    Keep track of initial light weights in PathTracer when SampleSurface is used

--- ilium/particle_tracer.go
+++ ilium/particle_tracer.go
@@ -559,8 +559,7 @@ func (pt *ParticleTracer) SampleLightPath(
 		case TRACER_UNIFORM_WEIGHTS:
 			weightTracker.AddP(0, 1)
 		case TRACER_POWER_WEIGHTS:
-			// TODO(akalin): Also include spatial pdf.
-			weightTracker.AddP(0, pChooseLight)
+			weightTracker.AddP(0, pChooseLight*pdfSpatial)
 		}
 
 		LeSpatialDivPdf.ScaleInv(&LeSpatialDivPdf, pChooseLight)
@@ -572,9 +571,9 @@ func (pt *ParticleTracer) SampleLightPath(
 			nSurface, Vector3{}, &LightMaterial{light, pSurface},
 			records)
 
-		wo, LeDirectionalDivPdf, pdf := light.SampleDirection(
-			lightBundle, pSurface, nSurface)
-		if LeDirectionalDivPdf.IsBlack() || pdf == 0 {
+		wo, LeDirectionalDivPdf, pdfDirectional :=
+			light.SampleDirection(lightBundle, pSurface, nSurface)
+		if LeDirectionalDivPdf.IsBlack() || pdfDirectional == 0 {
 			return records
 		}
 
@@ -584,7 +583,7 @@ func (pt *ParticleTracer) SampleLightPath(
 		case TRACER_UNIFORM_WEIGHTS:
 			weightTracker.AddP(1, 1)
 		case TRACER_POWER_WEIGHTS:
-			weightTracker.AddP(1, pdf)
+			weightTracker.AddP(1, pdfDirectional)
 		}
 
 		ray = Ray{pSurface, wo, pSurfaceEpsilon, infFloat32(+1)}
